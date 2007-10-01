9856ca89a552c683cc3f069317d69a886a4c8493
Jos Boumans <kane@cpan.org>
2007-10-01 15:41:30 +0000

    r2137@coke:  josboum | 2007-09-29 17:28:48 +0200  * some refactoring and abstracting out  * commands based on search index now work
    git-svn-id: http://oss.dwim.org/cpanplus-devel@2002 4dccba1d-3c1b-0410-aec4-feb514a8dabc

--- lib/CPANPLUS/Shell/Default/Plugins/CustomSource.pm
+++ lib/CPANPLUS/Shell/Default/Plugins/CustomSource.pm
@@ -13,88 +13,115 @@ sub plugins {
 }
 
 ### XXX Refactor
-### XXX use search numbers
 ### XXX note saving tree to make changes permanent
+
+my $Cb;
+my $Shell;
+my @Index   = ();
+
+sub _uri_from_cache {
+    my $self    = shift;
+    my $input   = shift;
+
+    ### you gave us a search number    
+    my $uri = $input =~ /^\d+$/    
+                ? $Index[ $input - 1 ] # remember, off by 1!
+                : $input;
+
+    my %files = reverse $Cb->list_custom_sources;
+
+    ### it's an URI we know
+    if( my $local = $files{ $uri } ) {
+        return wantarray 
+            ? ($uri, $local)
+            : $uri;
+    }
+    
+    ### couldn't resolve the input
+    error(loc("Unknown URI/index: '%1'", $input));
+    return;
+}
+
+sub _list_custom_sources {
+    my $class = shift;
+    
+    my %files = $Cb->list_custom_sources;
+    
+    $Shell->__print( loc("Your remote sources:"), $/ ) if keys %files;
+    
+    my $i = 0;
+    while(my($local,$remote) = each %files) {
+        $Shell->__printf( "   [%2d] %s\n", ++$i, $remote );
+
+        ### remember, off by 1!
+        push @Index, $remote;
+    }
+    
+    $Shell->__print( $/ );
+}
+
+sub _list_contents {
+    my $class = shift;
+    my $input = shift;
+
+    my ($uri,$local) = $class->_uri_from_cache( $input );
+    unless( $uri ) {
+        error(loc("--contents needs URI parameter"));
+        return;
+    }        
+
+    my $fh = OPEN_FILE->( $local ) or return;
+
+    $Shell->__printf( "   %s", $_ ) for sort <$fh>;
+    $Shell->__print( $/ );
+}
+
 sub custom_source {
     my $class   = shift;
-    my $shell   = shift;
-    my $cb      = shift;
+    my $shell   = shift;    $Shell  = $shell;   # available to all methods now
+    my $cb      = shift;    $Cb     = $cb;      # available to all methods now
     my $cmd     = shift;
     my $input   = shift || '';
     my $opts    = shift || {};
 
     ### show a list
     if( $opts->{'list'} ) {
-        my %files = $cb->list_custom_sources;
-        
-        $shell->__print( loc("Your remote sources:"), $/ ) if keys %files;
-        
-        my $i = 0;
-        while(my($local,$remote) = each %files) {
-            $shell->__printf( "   [%2d] %s\n", ++$i, $remote );
-        }
-        
-        $shell->__print( $/ );
+        $class->_list_custom_sources;
 
-    ### XXX make me work on search numbers        
     } elsif ( $opts->{'contents'} ) {
-        
-        unless( $input ) {
-            error(loc("--contents needs URI parameter"));
-            return;
-        }        
-
-        my %files = reverse $cb->list_custom_sources;
-        
-        my $local = $files{ $input } or (
-            error(loc("'%1' is not a known remote source", $input)),
-            return
-        );
-        
-        my $fh = OPEN_FILE->( $local ) or return;
-
-        $shell->__printf( "   %s", $_ ) for sort <$fh>;
-        $shell->__print( $/ );
-
+        $class->_list_contents( $input );
     
     } elsif ( $opts->{'add'} ) {        
         unless( $input ) {
             error(loc("--add needs URI parameter"));
             return;
-        }
+        }        
         
         $cb->add_custom_source( uri => $input ) 
-            and $shell->__print(loc("Added remote source '%1'", $input));
+            and $shell->__print(loc("Added remote source '%1'", $input), $/);
         
-        ### XXX list the contents
-        
+        $Shell->__print($/, loc("Remote source contains:"), $/, $/);
+        $class->_list_contents( $input );
         
     } elsif ( $opts->{'remove'} ) {
-        unless( $input ) {
+        my($uri,$local) = $class->_uri_from_cache( $input );
+        unless( $uri ) {
             error(loc("--remove needs URI parameter"));
             return;
-        }
+        }        
     
-        my %files = reverse $cb->list_custom_sources;
-        
-        my $local = $files{ $input } or (
-            error(loc("'%1' is not a known remote source", $input)),
-            return
-        );
+        1 while unlink $local;    
     
-        1 while unlink $local;
-    
-    
-        $shell->__print( loc("Removed remote source '%1'", $input) );
+        $shell->__print( loc("Removed remote source '%1'", $uri), $/ );
 
     ### XXX support single uri too
     } elsif ( $opts->{'update'} ) {
         $cb->update_custom_source 
-            and $shell->__print(loc("Updated remote sources"));      
+            and $shell->__print(loc("Updated remote sources"), $/);      
 
     } elsif ( $opts->{'write'} ) {
-        $cb->write_custom_source_index( path => $input )
-            and $shell->__print(loc("Wrote remote source index for '%1'", $input));              
+        $cb->write_custom_source_index( path => $input ) and
+            $shell->__print(loc("Wrote remote source index for '%1'", $input), $/);              
             
     } else {
         error(loc("Unrecognized command, see '%1' for help", '/? cs'));
@@ -109,12 +136,12 @@ sub custom_source_help {
         '    # Plugin to manage custom sources from the default shell'  . $/ .
         "    # See the 'CUSTOM MODULE SOURCES' section in the "         . $/ .
         '    # CPANPLUS::Backend documentation for details.'            . $/ .
-        '    /cs --list             # list available sources'           . $/ .
-        '    /cs --add       URI    # add source'                       . $/ .
-        '    /cs --remove    URI    # remove source'                    . $/ .
-        '    /cs --contents  URI    # show packages from source'        . $/ .
-        '    /cs --update   [URI]   # update source index'              . $/ .
-        '    /cs --write     PATH   # write source index'               . $/ 
+        '    /cs --list                     # list available sources'   . $/ .
+        '    /cs --add       URI            # add source'               . $/ .
+        '    /cs --remove    URI | INDEX    # remove source'            . $/ .
+        '    /cs --contents  URI | INDEX    # show packages from source'. $/ .
+        '    /cs --update   [URI | INDEX]   # update source index'      . $/ .
+        '    /cs --write     PATH           # write source index'       . $/ 
     );        
 
 }
