68d57bc0e6fc60924a8c16d76ae061fbc48c6a37
Jos Boumans <kane@cpan.org>
2008-01-08 16:04:14 +0000

    r4681@coke:  josboum | 2008-01-08 15:18:15 +0100  * YAML::Tiny is part of the core prereqs now, no longer needed to list specifically for test reporting
    git-svn-id: http://oss.dwim.org/cpanplus-devel@2266 4dccba1d-3c1b-0410-aec4-feb514a8dabc

--- lib/CPANPLUS/Selfupdate.pm
+++ lib/CPANPLUS/Selfupdate.pm
@@ -83,10 +83,7 @@ CPANPLUS::Selfupdate
                 sub { return 1 },   # always enabled
             ],            
             cpantest        => [
-                {
-                    'YAML::Tiny'     => '0.0',
-                    'Test::Reporter' => '1.34',
-                },
+                { 'Test::Reporter' => '1.34' },
                 sub { 
                     my $cb = shift;
                     return $cb->configure_object->get_conf('cpantest');
