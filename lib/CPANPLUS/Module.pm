54a879eddcc95ceb716f6ef15ef06358a1b8798f
Jos Boumans <kane@cpan.org>
2008-12-17 15:14:47 +0000

    r5995@rimmer:  kane | 2008-12-17 16:05:32 +0100  * Address: #41157: Module::module_is_supplied_with_perl_core() broken for perl 5.10
    git-svn-id: http://oss.dwim.org/cpanplus-devel@2488 4dccba1d-3c1b-0410-aec4-feb514a8dabc

--- lib/CPANPLUS/Module.pm
+++ lib/CPANPLUS/Module.pm
@@ -470,7 +470,12 @@ L<Module::ThirdParty> for more details.
 
         ### check Module::CoreList to see if it's a core package
         require Module::CoreList;
-        my $core = $Module::CoreList::version{ $ver }->{ $name };
+        
+        ### Address #41157: Module::module_is_supplied_with_perl_core() 
+        ### broken for perl 5.10: Module::CoreList's version key for the 
+        ### hash has a different number of trailing zero than $] aka
+        ### $PERL_VERSION.
+        my $core = $Module::CoreList::version{ 0+$ver }->{ $name };
 
         return $core;
     }
