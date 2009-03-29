44391a0f8485f0662e43389b91de0d1dfe12a140
Jos Boumans <kane@cpan.org>
2009-03-29 11:59:35 +0000

    r2819@coke:  kane | 2009-03-29 12:28:28 +0000  * test _add_to_includepath
    git-svn-id: http://oss.dwim.org/cpanplus-devel@2552 4dccba1d-3c1b-0410-aec4-feb514a8dabc

--- lib/CPANPLUS/Internals.pm
+++ lib/CPANPLUS/Internals.pm
@@ -427,11 +427,11 @@ sub _add_to_includepath {
     ### only add if it's not added yet
     for my $lib (@$dirs) {
         push @INC, $lib unless grep { $_ eq $lib } @INC;
-    
+
         ### it will be complaining if $ENV{PERL5LIB] is not defined (yet).   
         local $^W;  
         $ENV{'PERL5LIB'} .= $s . $lib 
-            unless $ENV{'PERL5LIB'} =~ qr|\Q$s$ENV{PERL5LIB}\E|;
+            unless $ENV{'PERL5LIB'} =~ qr|\Q$s$lib\E|;
     }
 
     return 1;
