a0bf8fead2d533efc422cb6a1d5b598fac6727f2
Chris Williams <chris@bingosnet.co.uk>
2009-12-23 09:58:21 +0000

    Bump version to 0.9001
    git-svn-id: http://oss.dwim.org/cpanplus-devel@2750 4dccba1d-3c1b-0410-aec4-feb514a8dabc

--- lib/CPANPLUS/Shell/Default.pm
+++ lib/CPANPLUS/Shell/Default.pm
@@ -26,7 +26,7 @@ local $Data::Dumper::Indent     = 1; # for dumpering from !
 BEGIN {
     use vars        qw[ $VERSION @ISA ];
     @ISA        =   qw[ CPANPLUS::Shell::_Base::ReadLine ];
-    $VERSION = "0.90";
+    $VERSION = "0.9001";
 }
 
 load CPANPLUS::Shell;
