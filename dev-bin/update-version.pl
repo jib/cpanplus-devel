a8f04bfdde3ad5d44656df4a531b6ae8958a18ea
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2011-03-19 23:49:11 +0000

    Bump version to 0.9103

--- dev-bin/update-version.pl
+++ dev-bin/update-version.pl
@@ -1,5 +1,5 @@
 my $Ver = shift or die usage();
-my $Scm = shift || 'svk';
+my $Scm = shift || 'git';
 
 my %Files = (
     qq[s/VERSION\\s*=.*?;/VERSION = "$Ver";/]
@@ -7,22 +7,22 @@ my %Files = (
                 lib/CPANPLUS/Internals.pm
                 lib/CPANPLUS/Shell/Default.pm
             ]],
-    qq[s/^version:.*\$/version: $Ver/]                  
+    qq[s/^version:.*\$/version: $Ver/]
         => [qw[ META.yml]],
-);        
+);
 
 #map { system( "p4 edit $_" ) } map { @$_ } values %Files;
 
 while( my($re,$aref) = each %Files ) {
 
     for my $file (@$aref) {
-    
+
         my $cmd = qq[$^X -pi -e'$re'];
         print "Running [$cmd $file]\n";
 
         system( "$cmd $file" );
     }
-}    
+}
 
 system("$Scm diff | less");
 system("$Scm commit" . ($Scm eq 'git' ? ' -a' : ''));
@@ -35,4 +35,4 @@ Usage:
     $0 NEW_VERSION [git]
 
     ];
-}    
+}
