f9df84b37d3a293bd87caa840fdc96d3b090da74
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2011-04-19 15:19:55 +0100

    Update version to 0.9104

--- lib/CPANPLUS/Internals.pm
+++ lib/CPANPLUS/Internals.pm
@@ -42,7 +42,7 @@ use vars qw[@ISA $VERSION];
             CPANPLUS::Internals::Report
         ];
 
-$VERSION = "0.9103";
+$VERSION = "0.9104";
 
 =pod
 
--- lib/CPANPLUS/Shell/Default.pm
+++ lib/CPANPLUS/Shell/Default.pm
@@ -26,7 +26,7 @@ local $Data::Dumper::Indent     = 1; # for dumpering from !
 BEGIN {
     use vars        qw[ $VERSION @ISA ];
     @ISA        =   qw[ CPANPLUS::Shell::_Base::ReadLine ];
-    $VERSION = "0.9103";
+    $VERSION = "0.9104";
 }
 
 load CPANPLUS::Shell;
