80eb9a19b3e30cd684a3398ffd2042ac1e468147
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2012-03-04 12:14:45 +0000

    Update ChangeLog and bump to version 0.9120

--- lib/CPANPLUS/Shell/Default.pm
+++ lib/CPANPLUS/Shell/Default.pm
@@ -26,7 +26,7 @@ local $Data::Dumper::Indent     = 1; # for dumpering from !
 BEGIN {
     use vars        qw[ $VERSION @ISA ];
     @ISA        =   qw[ CPANPLUS::Shell::_Base::ReadLine ];
-    $VERSION = "0.9119";
+    $VERSION = "0.9120";
 }
 
 load CPANPLUS::Shell;
