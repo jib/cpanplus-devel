983005b086fc4b17cd9443cece7de7debbfc6c12
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2015-07-03 20:09:37 +0100

    Unshift onto @INC so it matches what actually happens with PERL5LIB

--- lib/CPANPLUS/Internals.pm
+++ lib/CPANPLUS/Internals.pm
@@ -426,7 +426,7 @@ sub _add_to_includepath {
 
     ### only add if it's not added yet
     for my $lib (@$dirs) {
-        push @INC, $lib unless grep { $_ eq $lib } @INC;
+        unshift @INC, $lib unless grep { $_ eq $lib } @INC;
         #
         ### it will be complaining if $ENV{PERL5LIB] is not defined (yet).
         local $^W;
